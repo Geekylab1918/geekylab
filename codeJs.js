8603a58f606858070ad04dc4b0cee092b28b50ef
Geekylab <geekylab1918@gmail.com>
2025-09-02 19:53:26 +0300

    Update codeJs.js

--- codeJs.js
+++ codeJs.js
@@ -3,14 +3,14 @@ const soundBtn = document.getElementById('soundBtn');
 const soundStatus = document.getElementById('soundStatus');
 
 function updateUI(isMuted) {
-  if (!soundBtn) return; // protecție dacă nu suntem pe index.html
+  if (!soundBtn) return; 
   soundBtn.textContent = isMuted ? '🔊' : '🔇';
   soundBtn.setAttribute('aria-pressed', String(!isMuted));
-  //soundStatus.textContent = 'Sunet: ' + (isMuted ? 'OPRIT' : 'PORNIȚ');
+
 }
 
 if (video && soundBtn) {
-  // Încearcă să ruleze video
+
   video.play().catch(() => {
     const kickstart = () => {
       video.play().finally(() => document.removeEventListener('click', kickstart));
@@ -18,7 +18,7 @@ if (video && soundBtn) {
     document.addEventListener('click', kickstart, { once: true });
   });
 
-  // Toggle sunet
+
   soundBtn.addEventListener('click', async () => {
     try {
       if (video.muted) {
@@ -37,3 +37,4 @@ if (video && soundBtn) {
 
   updateUI(video.muted);
 }
+
